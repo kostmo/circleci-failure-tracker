9d70977570c90d867ba637ab13e7524f9ba68bd4
Karl Ostmo <kostmo@gmail.com>
2019-05-05 01:02:30 -0700

    drop python code

--- app/sql/sqlbase.py
+++
@@ -1,8 +0,0 @@
-DB_NAME = "loganci"
-DB_USERNAME = "logan"
-DB_PASSWORD = "logan01"
-
-
-def get_conn(hostname):
-    import psycopg2
-    return psycopg2.connect(host=hostname, dbname=DB_NAME, user=DB_USERNAME, password=DB_PASSWORD)
