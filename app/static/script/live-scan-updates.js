d45b4f98ed4a907e0a2f49dd5c512d39b4fb9a88
Karl Ostmo <kostmo@gmail.com>
2019-05-28 16:51:23 -0700

    animate scanning

--- app/static/script/live-scan-updates.js
+++ app/static/script/live-scan-updates.js
@@ -1,76 +1,12 @@
-
 function gen_summary_nested_pie() {
 
 
-   $.getJSON('/api/summary', function (mydata) {
+	// Splice in transparent for the center circle
+	Highcharts.getOptions().colors.splice(0, 0, 'transparent');
 
-		// Splice in transparent for the center circle
-		Highcharts.getOptions().colors.splice(0, 0, 'transparent');
-
-		Highcharts.chart('container-visited-fraction', {
-
-		    chart: {
-			height: '100%'
-		    },
-		    title: {
-			text: 'Failure causes'
-		    },
-		credits: {
-		    enabled: false
-		},
-		    series: gen_sunburst_data_series(mydata),
-		    tooltip: {
-			headerFormat: "",
-			pointFormat: '<b>{point.value}</b> in <b>{point.name}</b>'
-		    }
-		});
-    });
-}
-
-function gen_sunburst_data_series(data) {
-
-		var chartdata = [{
-		    id: '0.0',
-		    parent: '',
-		    name: 'Failures'
-
-		}, {
-		    id: '1.1',
-		    parent: '0.0',
-		    name: 'Unvisited',
-		    value: data["failed_builds"] - data["visited_builds"],
-		}, {
-		    id: '1.2',
-		    parent: '0.0',
-		    name: 'Visited',
-		    value: data["visited_builds"],
-		}, {
-		    id: '2.1',
-		    parent: '1.2',
-		    name: 'Cause available',
-		    value: data["explained_failures"] - data["timed_out_steps"],
-		}, {
-		    id: '2.2',
-		    parent: '1.2',
-		    name: 'Cause unvailable',
-		    value: data["visited_builds"] - data["explained_failures"],
-		}, {
-		    id: '2.3',
-		    parent: '1.2',
-		    name: 'Timeouts',
-		    value: data["timed_out_steps"],
-		}, {
-		    id: '3.1',
-		    parent: '2.1',
-		    name: 'Match found',
-		    value: data["steps_with_a_match"],
-		},
-		];
-
-
-	return [{
+	var data_series = [{
 		type: "sunburst",
-		data: chartdata,
+		data: [],
 		allowDrillToNode: true,
 		cursor: 'pointer',
 		dataLabels: {
@@ -97,8 +33,98 @@ function gen_sunburst_data_series(data) {
 		}
 		]
 
-	    }];
+	}];
+
+	var chart = Highcharts.chart('container-visited-fraction', {
+
+	    chart: {
+		height: '100%'
+	    },
+	    title: {
+		text: 'Failure causes'
+	    },
+		credits: {
+		    enabled: false
+		},
+	    series: data_series,
+	    tooltip: {
+		headerFormat: "",
+		pointFormat: '<b>{point.value}</b> in <b>{point.name}</b>'
+	    }
+	});
+
+	refetch(chart);
+
+
+	$("#animate-checkbox").change(function(event) {
+		var checkbox = event.target;
+		if (checkbox.checked) {
+			refetch(chart);
+		}
+	});
+
+
 }
+
+function refetch(chart) {
+
+	$.getJSON('/api/summary', function (mydata) {
+
+		var chartdata = gen_sunburst_data(mydata);
+
+		chart.series[0].setData(chartdata, false);
+		chart.redraw(false);
+
+		if ( $('#animate-checkbox').is(":checked") ) {
+			var myVar = setTimeout(function() {refetch(chart);}, 1000);
+		}
+	});
+}
+
+
+function gen_sunburst_data(data) {
+
+	var chartdata = [{
+	    id: '0.0',
+	    parent: '',
+	    name: 'Failures'
+
+	}, {
+	    id: '1.1',
+	    parent: '0.0',
+	    name: 'Unvisited',
+	    value: data["failed_builds"] - data["visited_builds"],
+	}, {
+	    id: '1.2',
+	    parent: '0.0',
+	    name: 'Visited',
+	    value: data["visited_builds"],
+	}, {
+	    id: '2.1',
+	    parent: '1.2',
+	    name: 'Cause available',
+	    value: data["explained_failures"] - data["timed_out_steps"],
+	}, {
+	    id: '2.2',
+	    parent: '1.2',
+	    name: 'Cause unavailable',
+	    value: data["visited_builds"] - data["explained_failures"],
+	}, {
+	    id: '2.3',
+	    parent: '1.2',
+	    name: 'Timeouts',
+	    value: data["timed_out_steps"],
+	}, {
+	    id: '3.1',
+	    parent: '2.1',
+	    name: 'Match found',
+	    value: data["steps_with_a_match"],
+	},
+	];
+
+	return chartdata;
+}
+
 
 
 
