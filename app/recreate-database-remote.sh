e205822b78580cb590a0cbecc995d2715a544b1e
Karl Ostmo <kostmo@gmail.com>
2019-05-30 00:16:10 -0700

    .pgpass

--- app/recreate-database-remote.sh
+++ app/recreate-database-remote.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -xe
 
+# This requires a ~/.pgpass file to be set up!
+
 DB_HOSTNAME=circleci-failure-tracker.cmivczl9ccia.us-east-2.rds.amazonaws.com
 
-dropdb -U postgres -h $DB_HOSTNAME loganci
-psql -U postgres -h $DB_HOSTNAME < ../schema.sql
+dropdb --no-password -U postgres -h $DB_HOSTNAME loganci
+psql --no-password -U postgres -h $DB_HOSTNAME < ../schema.sql
