4b6dffc90a8f5bfafe1998abf49aff6c3cd50f48
Karl Ostmo <kostmo@gmail.com>
2019-11-18 10:25:22 -0800

    span quick setter buttons

--- app/static/script/flakiness-review.js
+++ app/static/script/flakiness-review.js
@@ -6,9 +6,9 @@ var end_picker;
 function gen_table(element_id, data_url, height_string) {
 
 	const column_list = [
-		{title: "Job", field: "name", width: 400,
+		{title: "Job", field: "name", width: 500,
 		},
-		{title: "Count", field: "y", width: 125,
+		{title: "Isolated failures", field: "y", width: 200,
 		},
 	];
 
@@ -34,13 +34,74 @@ function requery_table() {
 }
 
 
-function setOneWeekSpan(end_picker_initial_date) {
+// Sets the start date to the previous Sunday, and the
+// end date in the future (coming Sunday)
+function bounds_this_week() {
+
+	const start_picker_initial_date = getSunday(new Date());
+	setSpanDaysForward(start_picker_initial_date, 7)
+
+	requery_table();
+}
+
+
+// Ending at the most recent Sunday, show the 7 preceeding days
+function bounds_last_week() {
+
+	const end_picker_initial_date = getSunday(new Date());
+	setSpanDaysBackward(end_picker_initial_date, 7);
+
+	requery_table();
+}
+
+
+// Sets the end date in the future (tomorrow)
+function bounds_today() {
+
+	const start_picker_initial_date = new Date();
+
+	setSpanDaysForward(start_picker_initial_date, 1)
+
+	requery_table();
+}
+
+
+function bounds_yesterday() {
+
+	const end_picker_initial_date = new Date();
+	setSpanDaysBackward(end_picker_initial_date, 1);
+
+	requery_table();
+}
+
+
+// See https://stackoverflow.com/a/4156516/105137
+function getSunday(base_date) {
+
+	var day = base_date.getDay(),
+	diff = base_date.getDate() - day;
+	return new Date(base_date.setDate(diff));
+}
+
+
+function setSpanDaysForward(start_picker_initial_date, day_count) {
+
+	start_picker.setDate(start_picker_initial_date);
+
+
+	const end_picker_initial_date = new Date();
+	end_picker_initial_date.setDate(start_picker_initial_date.getDate() + day_count);
+
+	end_picker.setDate(end_picker_initial_date);
+}
+
+
+function setSpanDaysBackward(end_picker_initial_date, day_count) {
 
 	end_picker.setDate(end_picker_initial_date);
 
-
 	const start_picker_initial_date = new Date();
-	start_picker_initial_date.setDate(end_picker_initial_date.getDate() - 7);
+	start_picker_initial_date.setDate(end_picker_initial_date.getDate() - day_count);
 
 	start_picker.setDate(start_picker_initial_date);
 }
@@ -53,11 +114,6 @@ function main() {
 	end_picker = new Pikaday({ field: document.getElementById('datepicker-end') });
 
 
-	const end_picker_initial_date = new Date();
-	setOneWeekSpan(end_picker_initial_date);
-
-
-	requery_table();
-
+	bounds_this_week();
 }
 
