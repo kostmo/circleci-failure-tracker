456681b378e639a41a7da6259a374858c6008d72
Karl Ostmo <kostmo@gmail.com>
2020-05-08 15:48:47 -0700

    add eb config files

--- app/prepare-docker-base-image.sh
+++ app/prepare-docker-base-image.sh
@@ -3,6 +3,6 @@
 # This script should be re-run anytime a new third-party
 # dependency (i.e. a new Haskell package or package version) is introduced:
 
-echo "NOTE: This command can take several minutes and requires uninterupted network."
+echo "NOTE: This command can take several minutes and requires uninterrupted network."
 
 docker build -f Dockerfile-base --tag="karl-base" .
